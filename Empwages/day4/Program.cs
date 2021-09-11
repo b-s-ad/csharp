eb8f76db68cbb82f90d584cf8c6f7f2dcd744c42
b-s-ad <ahmedshehbazb@gmail.com>
2021-09-11 16:12:21 +0530

    UC6_CalculateWageTill100HrsOr20IsReached

--- Empwages/day4/Program.cs
+++ Empwages/day4/Program.cs
@@ -12,9 +12,7 @@ namespace day4
             //UC3_PartTimeEmpWage.PartTimeEmpWage();
             //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
             //UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
-            //UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
-            //UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
-            UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("MNC", 25, 2, 10);
+            UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
             Console.ReadKey();
         }
     }
