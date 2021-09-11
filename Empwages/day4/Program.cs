023e4bcab2b1c1ef5070b643f65e78e926920b06
b-s-ad <ahmedshehbazb@gmail.com>
2021-09-11 16:46:40 +0530

    UCLast_FinalSolution

--- Empwages/day4/Program.cs
+++ Empwages/day4/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using day4.UC9;
 using Day4.UC10;
+using Day4.UCLast_FinalSolution;
 
 namespace day4
 {
@@ -28,14 +29,24 @@ namespace day4
 
             #endregion UC9 ending*/
 
-            #region UC10
+            /*#region UC10
 
             EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
             empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
             empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
             empWageBuilder.computeEmpWage();
 
+            #endregion UC10 ending*/
+
+            #region FinalSolution
+
+            EmpWageBuilder empWageBuild = new EmpWageBuilder();
+            empWageBuild.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuild.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuild.computeEmpWage();
+
             #endregion
+
 
             Console.ReadKey();
         }
