e26748e07ec894adce7d982644e835c3f5fe8a5f
b-s-ad <ahmedshehbazb@gmail.com>
2021-09-11 16:38:16 +0530

    UC-9

--- Empwages/day4/Program.cs
+++ Empwages/day4/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using day4.UC9;
 
 namespace day4
 {
@@ -14,7 +14,19 @@ namespace day4
             //UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
             //UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
             //UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
-            UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("MNC", 25, 2, 10);
+            //UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("MNC", 25, 2, 10);
+
+            #region UC9
+
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+
+            #endregion
+
             Console.ReadKey();
         }
     }
