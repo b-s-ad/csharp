3ea774269c2f68b61d91716df4e1688a4e32cc03
b-s-ad <ahmedshehbazb@gmail.com>
2021-09-11 15:56:04 +0530

    UC1_EmployeeAttendance

---
+++ Empwages/day4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace day4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UC1_EmployeeAttendance.GetAttendance();
+            Console.ReadKey();
+        }
+    }
+}
