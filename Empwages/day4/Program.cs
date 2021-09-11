7782d851684e8d0dbfdc34b303175d674e941a4d
b-s-ad <ahmedshehbazb@gmail.com>
2021-09-11 16:10:08 +0530

    UC8_ComputeEmpWageForMultipleCompanies

--- Empwages/day4/Program.cs
+++ Empwages/day4/Program.cs
@@ -11,7 +11,10 @@ namespace day4
             //UC2_EmpWage.EmpWage();
             //UC3_PartTimeEmpWage.PartTimeEmpWage();
             //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
-            UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            //UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            //UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+            //UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("MNC", 25, 2, 10);
             Console.ReadKey();
         }
     }
