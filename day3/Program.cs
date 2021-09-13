d895e74e06e7e3773118d22bdccd80c9c07a9155
b-s-ad <ahmedshehbazb@gmail.com>
2021-09-13 15:50:50 +0530

    Welcome to Line Comparison Computation Program

---
+++ day3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            startup.Wel();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ day3/startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day3
+{
+    class startup
+    {
+        public static void Wel()
+        {
+            Console.WriteLine(" Welcome to Line Comparison Computation Program ");
+        }
+    }
+}
