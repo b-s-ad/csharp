f9e9af615ef4a3279a22ac49f63e868abe45d0d1
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-15 14:21:21 +0530

    Snake and Ladder Problem

---
+++ Day4_Ladder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day4_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder Problem");
+            Console.ReadLine();
+        }
+    }
+}
