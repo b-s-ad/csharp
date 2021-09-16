3f95303cd8cb6aca82898ded3c13c5957dcd3fbc
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-16 14:14:47 +0530

    Logical Programs

---
+++ Logical Programs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Logical_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Logical Programs");
+            Console.ReadLine();
+        }
+    }
+}
