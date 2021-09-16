d30e0bc1fed6d7f89bac7964a00eb14e679f6cd1
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-17 01:04:07 +0530

    Prime Number

---
+++ Logical Programs/Prime Number.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using static System.Console;
+
+
+namespace Logical_Programs
+{
+    class Prime_Number
+    {
+        public static void Prime()
+        {
+            Console.WriteLine("Program to check Prime Number or Not");
+            Console.WriteLine("\n\nEnter the Number to check Prime: ");
+            var ends = Convert.ToInt32(Console.ReadLine());
+            bool IsPrimes = true;
+            int start = 1, end=ends;
+
+            
+            Console.WriteLine($"\n\nThe prime numbers between {start} to {ends} are :");
+
+            var numbers =
+                Enumerable.Range(start, end - start)
+                    .Where(IsPrime)
+                    .Select(number => number)
+                    .ToList();
+
+            Console.WriteLine(string.Join(", ", numbers));
+
+            bool IsPrime(int number)
+            {
+                // local function
+                bool CalculatePrime(int value)
+                {
+                  var Allprime = Math.Sqrt(number);
+/*                    we start from small to big (2, 3, 4, 5, etc...)*/
+                    
+                    for (var i = 2; i <= Allprime; i++)
+                    {
+                        if (value % i == 0)
+                        {
+                            return false;
+                        }
+
+                    }
+
+/*                  Explore all factors
+                    so we may know this number is prime*/
+                    return true;
+                }
+
+                /*negative numbers are not be prime
+                only call  CalculatePrime for non-negative*/
+
+                return number > 1 && CalculatePrime(number);
+            }
+            // to check Input number is Prime or not 
+
+            for (int i = 2; i < ends / 2; i++)
+            {
+                if (ends % i == 0)
+                {
+                    IsPrimes = false;
+                    break;
+                }
+            }
+            if (IsPrimes)
+            {
+                Console.Write("\n\nThe Number is Prime :  \n" + ends);
+            }
+            else
+            {
+                Console.Write( "\n\nThe Number is not Prime : \n" + ends);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Logical Programs/Program.cs
+++ Logical Programs/Program.cs
@@ -8,11 +8,11 @@ namespace Logical_Programs
         {
 
             Console.WriteLine("Logical Programs\n");
-            /*Fibonacci_Series.Fibonacci();
+           /* Fibonacci_Series.Fibonacci();
             Perfect_Number.Perfect();
             Reverse_a_number.Reverse();
-            */
-            Simulate_Stopwatch.Time();
+            Simulate_Stopwatch.Time();*/
+            Prime_Number.Prime();
             Console.ReadLine();
         }
     }
