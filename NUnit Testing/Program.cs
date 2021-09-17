1f8d23ca3990609b7d35620c003432c85ecac120
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-18 00:04:02 +0530

    NUnit Testing

---
+++ NUnit Testing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace NUnit_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("\nPrograms for NUnit Testing\n");
+
+            ReadLine();
+        }
+    }
+}
