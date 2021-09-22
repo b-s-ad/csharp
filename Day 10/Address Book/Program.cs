e0e46b644250d05001e504b110f24cef57c46d97
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-23 02:23:12 +0530

    Welcome to Address Book Program

---
+++ Day 10/Address Book/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace AddressBook
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("\tWelcome to Address Book Program");
+            ReadLine();
+        }
+
+    }
+}
+
+// Day 10 : Start
