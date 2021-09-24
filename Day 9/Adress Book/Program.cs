76e6b318ffffbf2e505ab9320e185ea19d2bb429
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-24 12:52:50 +0530

    Day9 UC 4 : Ability to delete a person using person's name

--- Day 9/Adress Book/Program.cs
+++ Day 9/Adress Book/Program.cs
@@ -7,122 +7,123 @@ namespace AddressBook // Day 9 UC 4
 
 {
     class Program
-    { 
-    static void Main(string[] args)
     {
-
-        AddressBook addressBook = new AddressBook();
-
-        PromptUser();
-
-        void Menu()
+        static void Main(string[] args)
         {
-            WriteLine("\n\tWelcome to Address Book Program\n");
-            WriteLine("\nPlease Choose below Option\n\n");   
-            WriteLine("1.Add a New contact.\n");
-            WriteLine("2.View the list of All Contacts.\n");
-            WriteLine("3.Modidy a Existing Contact\n");
-            WriteLine("4.Remove Contact\n");
-            WriteLine("5.To Exit Now [or] Anytime.\n");
-        }
 
-        void UpdateAddressBook(string userInput)
-        {
-            string firstname = "";
-            string lastname = "";
-            string address = "";
-            string city = "";
-            string state = "";
-            string zip = "";
-            string phonenumber = "";
-            string email = "";
+            AddressBook addressBook = new AddressBook();
 
+            PromptUser();
 
-            switch (userInput.ToLower())
+            void Menu()
             {
-                case "1":
-                    Write("Enter a First name: ");
-                    firstname = ReadLine().Trim();
-                    switch (firstname)
-                    {
-                        case "5":
-                            break;
-                            default:
-                                Write("Enter a Last name: ");
-                            lastname = ReadLine().Trim();
-                            switch (lastname)
-                            {
-                                case "5":
-                                    break;
-                                default:
+                WriteLine("\n\tWelcome to Address Book Program : \n");
+                WriteLine("\nPlease Choose below Option :\n\n");
+                WriteLine("1.Add a New Contact\n");
+                WriteLine("2.View the List of All Contacts\n");
+                WriteLine("3.Modidy a Existing Contact\n");
+                WriteLine("4.Remove Contact\n");
+                WriteLine("\tNote = TYPE ?? To Exit Now [or] Anytime.\n");
 
-                                        Write("Enter an address: ");
+            }
+
+            void UpdateAddressBook(string userInput)
+            {
+                string firstname = "";
+                string lastname = "";
+                string address = "";
+                string city = "";
+                string state = "";
+                string zip = "";
+                string phonenumber = "";
+                string email = "";
+
+
+                switch (userInput.ToLower())
+                {
+                    case "1":
+                        Write("Enter the First Name : ");
+                        firstname = ReadLine().Trim();
+                        switch (firstname)
+                        {
+                            case "??":
+                                break;
+                            default:
+                                Write("Enter the Last Name : ");
+                                lastname = ReadLine().Trim();
+                                switch (lastname)
+                                {
+                                    case "??":
+                                        break;
+                                    default:
+
+                                        Write("Enter the Address : ");
                                         address = ReadLine().Trim();
                                         switch (address)
                                         {
-                                            case "5":
+                                            case "??":
                                                 break;
                                             default:
 
-                                                Write("Enter the City: ");
-                                    city = ReadLine().Trim();
-                                    switch (city)
-                                    {
-                                        case "5":
-                                            break;
-                                            default:
-                                            case "1":
-                                            Write("Enter the state: ");
-                                            state = ReadLine().Trim();
-                                            switch (state)
-                                            {
-                                                case "5":
-                                                    break;
-                                                default:
+                                                Write("Enter the City : ");
+                                                city = ReadLine().Trim();
+                                                switch (city)
+                                                {
+                                                    case "??":
+                                                        break;
+                                                    default:
+                                                    case "1":
+                                                        Write("Enter the State : ");
+                                                        state = ReadLine().Trim();
+                                                        switch (state)
+                                                        {
+                                                            case "??":
+                                                                break;
+                                                            default:
 
-                                                case "1":
-                                                    Write("Enter the Zip: ");
-                                                    zip = ReadLine().Trim();
-                                                    switch (zip)
-                                                    {
-                                                        case "5":
-                                                            break;
-                                                        default:
+                                                            case "1":
+                                                                Write("Enter the Zip : ");
+                                                                zip = ReadLine().Trim();
+                                                                switch (zip)
+                                                                {
+                                                                    case "??":
+                                                                        break;
+                                                                    default:
 
-                                                        case "1":
-                                                            Write("Enter the Phone NUmber: ");
-                                                            phonenumber = ReadLine().Trim();
-                                                            switch (phonenumber)
-                                                            {
-                                                                case "5":
-                                                                    break;
-                                                                default:
+                                                                    case "1":
+                                                                        Write("Enter the Phone Number : ");
+                                                                        phonenumber = ReadLine().Trim();
+                                                                        switch (phonenumber)
+                                                                        {
+                                                                            case "??":
+                                                                                break;
+                                                                            default:
 
-                                                                case "1":
-                                                                    Write("Enter a Email : ");
-                                                                    email = ReadLine().Trim();
-                                                                    switch (email)
-                                                                    {
-                                                                        case "5":
-                                                                            break;
-                                                                        default:
+                                                                            case "1":
+                                                                                Write("Enter the Email : ");
+                                                                                email = ReadLine().Trim();
+                                                                                switch (email)
+                                                                                {
+                                                                                    case "??":
+                                                                                        break;
+                                                                                    default:
 
-                                                                            addressBook.AddEntry(firstname, lastname, address, city, state, zip, phonenumber, email);
-                                                                            break;
-                                                                    }
-                                                                    break;
-                                                            }
-                                                            break;
-                                                    }
-                                                    break;
-                                            }
-                                            break;
-                                    }
-                                    break;
-                            }
-                            break;
-                    }
-                    break;
+                                                                                        addressBook.AddEntry(firstname, lastname, address, city, state, zip, phonenumber, email);
+                                                                                        break;
+                                                                                }
+                                                                                break;
+                                                                        }
+                                                                        break;
+                                                                }
+                                                                break;
+                                                        }
+                                                        break;
+                                                }
+                                                break;
+                                        }
+                                        break;
+                                }
+                                break;
                         }
                         break;
                     case "4":
@@ -130,7 +131,7 @@ namespace AddressBook // Day 9 UC 4
                         firstname = Console.ReadLine();
                         switch (firstname)
                         {
-                            case "quit":
+                            case "??":
                                 break;
                             default:
                                 addressBook.RemoveEntry(firstname);
@@ -140,29 +141,26 @@ namespace AddressBook // Day 9 UC 4
 
                     case "2":
                         WriteLine(addressBook.ViewContactsList());
-        break;
+                        break;
                     case "3":
-                        WriteLine("Please enter the name of the Contact you wish to update");
+                        WriteLine("Please enter the existing name of the Contact you wish to update/modify : ");
                         firstname = ReadLine();
                         addressBook.UpdateContact(firstname);
                         break;
 
-    }
-}
-        void PromptUser()
-        {
-            Menu();
-            string userInput = "";
-            while (userInput != "5")
+                }
+            }
+            void PromptUser()
             {
-                WriteLine("\n\nChoose Any Option [1|2|3|4|5] : \n\n");
+                Menu();
+                string userInput = "";
+                while (userInput != "??")
+                {
+                    WriteLine("\n\nChoose Any Option [1|2|3|4|??] : \n\n");
                     userInput = ReadLine().Trim();
-                UpdateAddressBook(userInput);
+                    UpdateAddressBook(userInput);
+                }
             }
         }
     }
 }
-}
-
-
-    
