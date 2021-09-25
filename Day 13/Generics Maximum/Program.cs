52dadad50fc7bc1b751e9aa39465f0d010156d2b
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-26 00:53:18 +0530

    UC 1 : Given 3 Integers find the maximum

--- Day 13/Generics Maximum/Program.cs
+++ Day 13/Generics Maximum/Program.cs
@@ -8,13 +8,18 @@ namespace FindMaxNum
         static void Main(string[] args)
         {
             Console.WriteLine("\tFind max Number\n");
+            WriteLine("Given Max Number at 1st Position return the Same Number : 33, 11, 22 ");
+            WriteLine("Given Max Number at 2nd Position return the Same Number : 22, 33, 11 ");
+            WriteLine("Given Max Number at 3rd Position return the Same Number : 22, 11, 33");
+            WriteLine("");
             int output = MaximumNumberCheck.MaximumIntegerNumber(33, 11, 22);
             Console.WriteLine(output);
-            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(22.45, 111.2, 78.56);
-            Console.WriteLine(doubleoutput);
-            string strigoutput = MaximumNumberCheck.MaximumStringNumber("22", "33", "44");
-            Console.WriteLine(strigoutput);
+            int output2 = MaximumNumberCheck.MaximumIntegerNumber(22, 33, 11);
+            Console.WriteLine(output2);
+            int output3 = MaximumNumberCheck.MaximumIntegerNumber3(22, 11, 33);
+            Console.WriteLine(output3);
             ReadLine();
+           
         }
 
     }
