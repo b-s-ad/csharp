42a8d5fdd8905497b1c02fb10d26ca1b3bdcd3ea
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-27 18:40:49 +0530

    UC 1 : Simple Linked List of 56, 30 and 70

---
+++ Day 14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region linked list
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            //list.InsertAtParticularPosition(2, 30);
+            WriteLine("");
+            list.Display();
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
