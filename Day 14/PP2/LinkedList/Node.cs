5c0cbe3db434f5e5c2cb9d85aa32a2036f6e5fe6
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-29 00:25:13 +0530

    UC 1 : Ability to create a Stack of 56->30->70

---
+++ Day 14/PP2/LinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    class Node //UC 9
+    {
+
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+
+}
---
+++ Day 14/PP2/Program.cs
@@ -0,0 +1,43 @@
+﻿using DataStructure.StackUsingLinkedList;
+using System;
+using static System.Console;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            ReadKey();
+            list.Display();
+            //list.InsertAtParticularPosition(2, 30);
+            WriteLine("\n");
+            #endregion
+        
+
+        #region  stackcode
+        LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            WriteLine("\n");
+            linkedListStack.Display();
+            WriteLine("");
+            ReadLine();
+            
+            //linkedListStack.Peek(); top.data
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+            #endregion stackcode
+
+        }
+
+    }
+}
