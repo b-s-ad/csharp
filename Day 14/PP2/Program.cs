ac271d734574d0e02f87c04252890398d5fbc915
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-09-29 02:15:22 +0530

    UC 3 : Queue

--- Day 14/PP2/Program.cs
+++ Day 14/PP2/Program.cs
@@ -1,4 +1,5 @@
-﻿using DataStructure.StackUsingLinkedList;
+﻿using DataStructure.QueueUsingLinkedList;
+using DataStructure.StackUsingLinkedList;
 using System;
 using static System.Console;
 
@@ -6,37 +7,48 @@ namespace DataStructure
 {
     class Program
     {
-        static void Main(string[] args) //Section 2 // UC 2 
+        static void Main(string[] args) //Section 2 // UC 3
         {
 
-          /*  #region
-             LinkedList list = new LinkedList();
-             list.Add(56);
-             list.Add(30);
-             list.Add(70);
-             ReadKey();
-             list.Display();
-             //list.InsertAtParticularPosition(2, 30);
-             WriteLine("\n");
-             #endregion
-         */
+            /*  #region
+               LinkedList list = new LinkedList();
+               list.Add(56);
+               list.Add(30);
+               list.Add(70);
+               ReadKey();
+               list.Display();
+               //list.InsertAtParticularPosition(2, 30);
+               WriteLine("\n");
+               #endregion
+           
 
-            #region  stackcode
-            LinkedListStack linkedListStack = new LinkedListStack();
-           linkedListStack.Push(70);
-            linkedListStack.Push(30);
-            linkedListStack.Push(56);
+              #region  stackcode
+              LinkedListStack linkedListStack = new LinkedListStack();
+             linkedListStack.Push(70);
+              linkedListStack.Push(30);
+              linkedListStack.Push(56);
+              WriteLine("\n");
+              linkedListStack.Display();
+              WriteLine("");
+              ReadLine();
+
+              linkedListStack.Peek(); //top.data
+              linkedListStack.Pop();
+              linkedListStack.IsEmpty();
+              linkedListStack.Display();
+              ReadLine();
+              #endregion stackcode
+  */
+
+            #region  Queue link
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
             WriteLine("\n");
-            linkedListStack.Display();
-            WriteLine("");
+            linkedListQueue.Display();// this.top=this.top.next
             ReadLine();
-
-            linkedListStack.Peek(); //top.data
-            linkedListStack.Pop();
-            linkedListStack.IsEmpty();
-            linkedListStack.Display();
-            ReadLine();
-            #endregion stackcode
+            #endregion
 
         }
 
---
+++ Day 14/PP2/QueueUsingLinkedList/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.QueueUsingLinkedList
+{
+    class Node //Section 2 // UC 3
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
