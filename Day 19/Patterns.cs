ca3ba1d7203166e1d20613fe527ff294d2f10e3f
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-01 21:39:31 +0530

    UC : 1 First name starts with Cap and has min 3 char

---
+++ Day 19/Patterns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPatternMatching
+{
+    class Patterns
+    {
+        public static string Regex_Pincode = "^([A-Z][a-zA-Z0-9]*)$";
+       
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, Regex_Pincode);
+        }
+    }
+
+}
---
+++ Day 19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace RegexPatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mainmenu();
+        }
+
+        static void Mainmenu()
+        {
+            Clear();
+            Patterns patterns = new Patterns();
+            WriteLine("\n\tUC 1\n" + "\n As a User need to enter a valid First Name"+ "\n-First name starts with Cap and has minimum 3 characters");
+            WriteLine("\nDefault Value : Bond007\n \nEnter Input : \n");
+            WriteLine(patterns.validatePinCode(ReadLine()));
+            WriteLine("\nPress Enter To Type Value  || 0 = To Exit");
+            
+
+            string Menu;
+            Menu = ReadLine();
+            switch (Menu)
+            {
+                case "0":
+                    System.Environment.Exit(1);
+                    break;
+
+                default:
+                    WriteLine("");
+                    break;
+            }
+            Mainmenu();
+        }
+    }
+
+}
