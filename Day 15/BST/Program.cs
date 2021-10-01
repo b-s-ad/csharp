807e8f970f3f80f2b305161849fcb4fc970a8cc1
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-01 20:23:12 +0530

    UC : 1

---
+++ Day 15/BST/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace BST
+{
+   
+        class Program
+        {
+            static void Main(string[] args)
+
+            {
+
+                Console.WriteLine("BST\n");
+                BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+
+                binarySearch.Insert(30);
+                binarySearch.Insert(70);
+                binarySearch.Insert(22);
+                binarySearch.Insert(40);
+                binarySearch.Insert(60);
+                binarySearch.Insert(95);
+                binarySearch.Insert(11);
+                binarySearch.Insert(65);
+                binarySearch.Insert(3);
+                binarySearch.Insert(16);
+                binarySearch.Insert(63);
+                binarySearch.Insert(67);
+                WriteLine("");
+                binarySearch.Display();
+                bool result = binarySearch.IfExists(63, binarySearch);
+                //WriteLine(result);
+                ReadLine();
+               
+
+            }
+        }
+    }
+
