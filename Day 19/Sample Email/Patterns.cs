f442987ed5630f1d5414ee9140bb3c0ce0be0e89
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-02 16:21:53 +0530

    UC 8.1 : abc@yahoo.com

---
+++ Day 19/Sample Email/Patterns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPatternMatching
+{
+    class Patterns
+    {
+        public static string Regex_VE1 = "^[a-zA-z]+[@}+[a-zA-Z]+[.][a-zA-Z]{2,3}$"; 
+       
+        public bool validateVE1(string VE1)
+        {
+            return Regex.IsMatch(VE1, Regex_VE1);
+        }
+    }
+
+}
---
+++ Day 19/Sample Email/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+
+namespace RegexPatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mainmenu();
+        }
+
+        static void Mainmenu()
+        {
+            Clear();
+            Patterns patterns = new Patterns();
+            WriteLine("\n\tUC 8\n" + "\nSample Emails to Test\n");
+            WriteLine("A. Valid Emails 01");
+            WriteLine("\nDefault Value : abc@yahoo.com \n");
+            WriteLine(patterns.validateVE1("abc@yahoo.com"));
+            WriteLine("\nEnter New Valid Input : \n");
+            WriteLine(patterns.validateVE1(ReadLine()));
+            WriteLine("\nPress Enter To Type Value  || 0 = To Exit");
+            
+
+            string Menu;
+            Menu = ReadLine();
+            switch (Menu)
+            {
+                case "0":
+                    System.Environment.Exit(1);
+                    break;
+
+                default:
+                    WriteLine("");
+                    break;
+            }
+            Mainmenu();
+        }
+    }
+
+}
