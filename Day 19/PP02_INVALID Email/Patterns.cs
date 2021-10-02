e32d0f97ae62cc71eb3b373afd2abddd0e3c284c
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-02 17:37:59 +0530

    UC 9.1 IE :  abc – must contains “@” symbol

---
+++ Day 19/PP02_INVALID Email/Patterns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPatternMatching
+{
+    class Patterns
+    {
+        public static string Regex_IVE1 = "^[a-zA-Z]+[@]$";
+
+        public bool validateIVE1(string IVE1)
+        {
+            return Regex.IsMatch(IVE1, Regex_IVE1);
+        }
+    }
+
+}
---
+++ Day 19/PP02_INVALID Email/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace RegexPatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mainmenu();
+        }
+
+        static void Mainmenu()
+        {
+            Clear();
+            Patterns patterns = new Patterns();
+            WriteLine("\n\tUC 9\n" + "\nSample Emails to Test\n");
+            WriteLine("B. InValid Emails 01");
+            WriteLine("\nDefault Value : abc – must contains “@” symbol");
+            WriteLine(patterns.validateIVE1("abc"));
+            WriteLine("\nValid Value : abc@");
+            WriteLine(patterns.validateIVE1("abc@"));
+            WriteLine("\nEnter New Valid |or| InValid Input : \n");
+            WriteLine(patterns.validateIVE1(ReadLine()));
+            WriteLine("\nPress Enter To Type Value  || 0 = To Exit");
+            
+
+            string Menu;
+            Menu = ReadLine();
+            switch (Menu)
+            {
+                case "0":
+                    System.Environment.Exit(1);
+                    break;
+
+                default:
+                    WriteLine("");
+                    break;
+            }
+            Mainmenu();
+        }
+    }
+
+}
